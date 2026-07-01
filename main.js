ed03bb1281bd33f47e1e05f4b12183fc4b3b21b7
Jaqueline Grandi <cardosojaqueline.t@gmail.com>
2026-07-01 02:08:13 +0200

    update CSS hamburguesa

--- main.js
+++ main.js
@@ -6,15 +6,23 @@ const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
 const navMenu = document.getElementById("nav-menu");
 
+const aboutToggle = document.getElementById("about-toggle");
+const aboutBack = document.getElementById("about-back");
+const aboutDropdown = document.getElementById("about-dropdown");
+
+// ===============================
+// ABRIR MENÚ
+// ===============================
+
 if (openMenu && closeMenu && navMenu) {
 
-    openMenu.addEventListener("click", () => {
+    openMenu.addEventListener("click", function () {
         navMenu.classList.add("visible");
         openMenu.classList.add("hide");
         closeMenu.style.display = "block";
     });
 
-    closeMenu.addEventListener("click", () => {
+    closeMenu.addEventListener("click", function () {
         navMenu.classList.remove("visible");
         openMenu.classList.remove("hide");
         closeMenu.style.display = "none";
@@ -22,29 +30,25 @@ if (openMenu && closeMenu && navMenu) {
         if (aboutDropdown) {
             aboutDropdown.classList.remove("active");
         }
+
     });
 
 }
 
-
 // ===============================
-// ABOUT SUBMENU
+// SUBMENÚ ABOUT
 // ===============================
-
-const aboutToggle = document.getElementById("about-toggle");
-const aboutBack = document.getElementById("about-back");
-const aboutDropdown = document.getElementById("about-dropdown");
 
 if (aboutToggle && aboutBack && aboutDropdown) {
 
-    aboutToggle.addEventListener("click", (e) => {
+    aboutToggle.addEventListener("click", function (e) {
         e.preventDefault();
         e.stopPropagation();
 
         aboutDropdown.classList.add("active");
     });
 
-    aboutBack.addEventListener("click", (e) => {
+    aboutBack.addEventListener("click", function (e) {
         e.preventDefault();
         e.stopPropagation();
 
